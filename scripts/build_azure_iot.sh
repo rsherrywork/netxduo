07fb4f65f73aa13b8bc0d10a6fa74cf220b4e75e
Frédéric Desbiens <frederic.desbiens@eclipse-foundation.org>
2026-03-06 23:53:52 +0100

    Restored build and run scripts to their prior versions.

--- scripts/build_azure_iot.sh
+++ scripts/build_azure_iot.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-RUN_SCRIPT="$SCRIPT_DIR/../test/cmake/azure_iot/run.sh"
-
-. ./func_build.sh
+$(dirname `realpath $0`)/../test/cmake/azure_iot/run.sh build all
--- scripts/build_mqtt_interoperability.sh
+++ scripts/build_mqtt_interoperability.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-RUN_SCRIPT="$SCRIPT_DIR/../test/cmake/mqtt_interoperability/run.sh"
-
-. ./func_build.sh
+$(dirname `realpath $0`)/../test/cmake/mqtt_interoperability/run.sh build all
